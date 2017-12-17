4d62ccc462a885143b8a976f683ca9ca4f922b7d
datner <yuval.datner.alt@gmail.com>
2017-12-17 21:51:56 +0200

    Bulma added and app setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bulma-0.6.1/css/bulma.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="box">
-          test 123
+      <div className="App hero">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              write area
+            </div>
+            <div className="column">
+              read area
+            </div>
+          </div>
         </div>
       </div>
     );
